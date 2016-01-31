26797fb2c5717ed5c36d79797459a5b5c77cda9f
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2016-01-31 11:43:34 +0100

    Implementation of rawTextToMarkupText methods

--- project/m2dl.osgi.javabundle/src/m2dl/osgi/javabundle/JavaDecorator.java
+++ project/m2dl.osgi.javabundle/src/m2dl/osgi/javabundle/JavaDecorator.java
@@ -3,11 +3,41 @@ package m2dl.osgi.javabundle;
 import m2dl.osgi.apidecoratorbundle.LanguageDecoratorService;
 
 public class JavaDecorator implements LanguageDecoratorService {
-
+	// Only some keywoards, we cann add more
+	private static final String[] KEY_WORDS = 
+		{"package", "if", "while", "void", "return", "public", "private", "protected", "class", "enum"};
+	
+	 
+	private String key(String key) {
+		return key.replace(key, ":keyword{"+key+"}");
+	}
+	
+	/**
+	 * Take a text with rawtext in parameters and return an html text with keywords in color
+	 * @param markup A text with markup keywords
+	 * @return An html text with colors
+	 */
 	@Override
 	public String htmlColorString(String raw) {
 		System.out.println("tata");
 		return null;
+	}
+	
+	/**
+	 * Take a raw text in parameter, and return a text with markup keyword around keywords.
+	 * @param raw The raw text
+	 * @return A text with keywords
+	 */
+	@Override
+	public String rawTextToMarkupText(String raw) {
+		String ret = "";
+		for(String s : KEY_WORDS) {
+			ret = ret.replace(s, key(s));
+		}
+		
+		// Comments replacing
+		ret = ret.replaceAll("(?:/\\*(?:[^*]|(?:\\*+[^*/]))*\\*+/)|(?://.*)", ":comment{$1}");
+		return ret;
 	}
 
 }
